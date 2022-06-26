b31158ac7bde1d09b2c41ce6f48bd01f04982dc0
croco_die 99 <ahmad.syahir@gameconomy.tech>
2022-06-26 19:54:54 +0800

    add thank you message

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ package SCSJ4383_PS1;
 public class helloworld{
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("\nThank you for visiting");
     }
 }
